d6624218280041196f89270c081361b2d7d7ee37
gst <gustavo_souza12@estudante.sesisenai.org.br>
2024-10-25 15:19:09 -0300

    salvo

--- Tabuada/js/scripts.js
+++ Tabuada/js/scripts.js
@@ -1,1 +1,46 @@
-console.log("Teste");
+// Seleção de Elementos
+
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicador")
+
+const multiplicationTable = document.querySelector("#multiplication-operations")
+
+// Funções
+
+const createTable = (number, multiplicatorNumber) =>{
+
+    multiplicationTable.innerHTML = "";
+
+    for(i = 1; i <= multiplicatorNumber; i ++){
+        const result = number * i;
+        
+        const template = `<div class="row">
+                        <div class="operation">${number} x ${i} =</div>
+                        <div class="result">${result}</div>
+                        </div>`;
+
+        const parser = new DOMParser();
+
+        const htmlTemplate = parser.parseFromString(template, "text/html");
+
+        const row = htmlTemplate.querySelector(".row");
+
+        multiplicationTable.appendChild(row);
+    }
+
+}
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if(!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
+
+})
